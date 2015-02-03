69f3fc2db4c062b033d15672a6299ab2fe538102
anne <ahbauer01@gmail.com>
2015-02-03 14:59:39 -0500

    backup h1b.ipynb

--- web/app/predict_h1b.py
+++ web/app/predict_h1b.py
@@ -11,9 +11,9 @@ import seaborn
 
 
 def predict_nH1B(date=pandas.to_datetime('2014-02-01')):
-    regr = joblib.load('/Users/bauer/insight/teleborder/data/my_model.pkl')
-    (sents_model, sp500_model, empl_model, trend_model) = joblib.load('/Users/bauer/insight/teleborder/data/xdata.pkl')
-    norms = joblib.load('/Users/bauer/insight/teleborder/data/norms.pkl')
+    regr = joblib.load('/Users/bauer/insight/teleborder/web/data/my_model.pkl')
+    (sents_model, sp500_model, empl_model, trend_model) = joblib.load('/Users/bauer/insight/teleborder/web/data/xdata.pkl')
+    norms = joblib.load('/Users/bauer/insight/teleborder/web/data/norms.pkl')
     month_flags = [1,1,4,1,1,2,1,1,1,1,1,1]
     
     # print sents_model
@@ -41,11 +41,11 @@ def predict_nH1B(date=pandas.to_datetime('2014-02-01')):
     prediction = regr.predict(np.array([x1,x2,x3,x4,x5,x6,x7,x8,x9,x10]).T)
     prediction = (norms[1]*prediction + norms[0])[0]
     
-    return prediction, [25.56,22.10,18.80,18.19], norms[4:]
+    return prediction, norms[4:]
 
 def make_figure(date=pandas.to_datetime('2014-02-01'), pred=80000):
     
-    (prediction_series,month_series) = joblib.load('/Users/bauer/insight/teleborder/data/pred.pkl')
+    (prediction_series,month_series) = joblib.load('/Users/bauer/insight/teleborder/web/data/pred.pkl')
     # fig = plt.Figure()
     # ax0 = fig.add_subplot(111)
     # ax0.plot(month_series['2005':].values, month_series['2005':].index,label='H1B Rate')
@@ -58,12 +58,13 @@ def make_figure(date=pandas.to_datetime('2014-02-01'), pred=80000):
     ax0 = None
     fig = plt.figure()
     #ax0 = fig.add_subplot(111)
+    plt.axvline(date + datetime.timedelta(days=30),color='black')
     month_series['2005':].plot(label='H1B Rate',color='blue')
-    prediction_series.plot(label='Prediction',color='red')
-    ax0 = point_series.plot(label='Your Date',style='go',markersize=10)
+    ax0 = prediction_series.plot(label='Prediction',color='red')
+    #ax0 = point_series.plot(label='Your Date',style='go',markersize=10)
     ax0.set_xlabel('Date')
     ax0.set_ylabel('Number of H1B Applications')
-    ax0.legend()
+    ax0.legend(loc=9)
     #fig = ax0.get_figure()
     #os.system('rm /Users/bauer/insight/teleborder/web/app/static/imgs/dynamicfig.png')
     fig.savefig('/Users/bauer/insight/teleborder/web/app/static/imgs/dynamicfig.png')
